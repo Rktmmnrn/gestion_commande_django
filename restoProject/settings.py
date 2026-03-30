44e2935550096b014a4d44b4ec5cc55e947750fb
Rktmmnrn <rfanomezaniavo@gmail.com>
2026-03-30 19:56:53 +0300

    feat: ajout methode

--- restoProject/settings.py
+++ restoProject/settings.py
@@ -125,9 +125,19 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8080",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 REST_FRAMEWORK={
     'DEFAULT_PERMISSION_CLASSES':[
