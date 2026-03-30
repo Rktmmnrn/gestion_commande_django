1663bc2b9602c47343050014bb4a11df908de994
Rktmmnrn <rfanomezaniavo@gmail.com>
2026-03-30 17:07:13 +0300

    feat: correction bug

--- restoProject/settings.py
+++ restoProject/settings.py
@@ -121,9 +121,13 @@ STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8080",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK={
     'DEFAULT_PERMISSION_CLASSES':[
