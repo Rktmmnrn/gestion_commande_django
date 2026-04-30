20dba65c16952c25f4eab5d91709c5734e60f569
Rktmmnrn <rfanomezaniavo@gmail.com>
2026-04-30 18:02:06 +0300

    feat: clean code

--- orders/views.py
+++ orders/views.py
@@ -69,66 +69,6 @@ class OrderViewSet(viewsets.ModelViewSet):
         
         serializer = self.get_serializer(order)
         return Response(serializer.data)
-    
-    # @action(detail=True, methods=['post'], url_path='add_item')
-    # def add_item(self, request, pk=None):
-    #     order = self.get_object()
-    #     product_id = request.data.get('product')
-    #     quantity = request.data.get('quantity', 1)
-
-    #     # validation des quantités
-    #     try:
-    #         quantity = int(quantity)
-    #         if quantity <= 0:
-    #             return Response (
-    #                 {'error': 'Quantity must be positive'},
-    #                 status=status.HTTP_400_BAD_REQUEST
-    #             )
-    #     except ValueError:
-    #         return Response (
-    #             {'error': 'Quantity must be an integer'},
-    #             status=status.HTTP_400_BAD_REQUEST
-    #         )
-        
-    #     # Vérifier que le produit existe
-    #     try:
-    #         product = Product.objects.get(id=product_id)
-    #     except Product.DoesNotExist:
-    #         return Response(
-    #             {'error': f'Product with id {product_id} does not exist'},
-    #             status=status.HTTP_404_NOT_FOUND
-    #         )
-        
-    #     # Vérifier que le produit est disponible
-    #     if not product.available:
-    #         return Response(
-    #             {'error': f'Product {product.name} is not available'},
-    #             status=status.HTTP_400_BAD_REQUEST
-    #         )
-        
-    #     # Vérifier si l'item existe déjà dans la commande
-    #     existing_item = OrderItem.objects.filter(order=order, product=product).first()
-        
-    #     if existing_item:
-    #         # Mettre à jour la quantité si l'item existe déjà
-    #         existing_item.quantity += quantity
-    #         existing_item.save()
-    #         item = existing_item
-    #     else:
-    #         # Créer un nouvel item
-    #         item = OrderItem.objects.create(
-    #             order=order,
-    #             product=product,
-    #             quantity=quantity,
-    #             price=product.price
-    #         )
-        
-    #     # Mettre à jour le champ updated_by de la commande
-    #     order.updated_by = request.user
-    #     order.save()
-
-    #     serializer = OrderItemSerializer(item)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
